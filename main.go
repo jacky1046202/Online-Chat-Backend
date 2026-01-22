bab1907b7ef6323fbf7e240d24fdcbf45ef39019
jacky1046202 <jacky1046202@gmail.com>
2026-01-22 22:49:48 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+)
+
+// 使用者
+type Client struct {
+	Hub  *Hub
+	Conn *websocket.Conn
+	Send chan []byte
+}
+
+// 聊天室
+type Hub struct {
+	Clients    map[*Client]bool
+	Broadcast  chan []byte
+	Register   chan *Client
+	Unregister chan *Client
+}
+
+// 建立聊天室
+func NewHub() *Hub {
+	return &Hub{
+		Clients:    make(map[*Client]bool),
+		Broadcast:  make(chan []byte),
+		Register:   make(chan *Client),
+		Unregister: make(chan *Client),
+	}
+}
+
+func (h *Hub) Run() {
+	for {
+		// 觀察聊天室狀況
+		select {
+
+		case client := <-h.Register:
+			h.Clients[client] = true
+
+		case client := <-h.Unregister:
+			if _, ok := h.Clients[client]; ok {
+				delete(h.Clients, client)
+				close(client.Send)
+			}
+
+		case message := <-h.Broadcast:
+			for client := range h.Clients {
+				select {
+				case client.Send <- message:
+				default:
+					close(client.Send)
+					delete(h.Clients, client)
+				}
+
+			}
+		}
+	}
+}
+
+// 讀取
+func (c *Client) ReadPump() {
+	defer func() {
+		c.Hub.Unregister <- c
+		c.Conn.Close()
+	}()
+
+	for {
+		_, message, err := c.Conn.ReadMessage()
+
+		if err != nil {
+			break
+		}
+
+		c.Hub.Broadcast <- message
+	}
+
+}
+
+// 寫入
+func (c *Client) WritePump() {
+	for message := range c.Send {
+		err := c.Conn.WriteMessage(websocket.TextMessage, message)
+
+		if err != nil {
+			break
+		}
+	}
+
+	c.Conn.Close()
+}
+
+// cors
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func main() {
+	hub := NewHub()
+
+	go hub.Run()
+
+	r := gin.Default()
+
+	r.GET("/nigg", func(c *gin.Context) {
+		conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+		if err != nil {
+			return
+		}
+
+		client := &Client{
+			Hub:  hub,
+			Conn: conn,
+			Send: make(chan []byte, 256),
+		}
+
+		client.Hub.Register <- client
+
+		go client.WritePump()
+		client.ReadPump()
+	})
+
+	r.Run(":6969")
+}
